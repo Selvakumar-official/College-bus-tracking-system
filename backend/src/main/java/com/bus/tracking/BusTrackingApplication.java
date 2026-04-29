a0b39c65d1076b785689f8cb9f3e3b068d61d677
SELVAKUMAR K <109788803+Selvakumar-official@users.noreply.github.com>
2026-04-29 16:09:52 +0530

    Create BusTrackingApplication.java

---
+++ backend/src/main/java/com/bus/tracking/BusTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.bus.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTrackingApplication.class, args);
+    }
+}
